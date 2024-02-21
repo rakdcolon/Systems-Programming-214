0db751904e6e525bd8944d0020d36a8f255ce9d9
Rohan Karamel <rak218@scarletmail.rutgers.edu>
2024-02-20 21:46:17 -0500

    MALLOC and FREE are done

--- Projects/My Little Malloc/mymalloc.c
+++ Projects/My Little Malloc/mymalloc.c
@@ -1,106 +1,116 @@
-#include <stdlib.h>
-#include <stdio.h>
+
+/*
+ *
+ * INCLUDE HEADER FILES
+ *
+ */
 #include "mymalloc.h"
 
-#define MEMORY_SIZE 512
-#define HEADER_SIZE sizeof(struct header)
-static double memory[MEMORY_SIZE];
 
-void *firstChunk = (header *)memory;
+/*
+ *
+ * MALLOC
+ *
+ */
+void *mymalloc(size_t size, char *FILE, int line) {
 
-void *mymalloc(size_t size, char *FILE, int line)
-{
-    header *head = firstChunk;
+    // If any of the parameters are wrong, then halt the program
+    if (size <= 0 || !FILE || line <= 0) { 
+        fprintf(stderr, "Error in %p on line %d: Invalid Size", FILE, line);
+        return NULL; // Here I am returning a NULL pointer, instead of an error similar to how real malloc works.
+    }
 
-    if (head->next == NULL)
-    {
-        if (size >= MEMORY_SIZE - sizeof(struct header))
-        {
-            fprintf(stderr, "\nError in %s line %d: No space available \n", FILE, line);
-            return NULL;
-        }
-        head->free = false;
+    size = NEXT_MULTIPLE_OF_EIGHT; // Need to offset size to the next multiple of 8 as each chunk is 8 bytes, so the size is rounded up always.
 
-        void *ptr = head;
+    int *head = START_OF_MEMORY; // Pointer to the beginning of the memory
 
-        if (MEMORY_SIZE - size <= HEADER_SIZE)
-        {
-            head->next = head;
-            head->size = MEMORY_SIZE - HEADER_SIZE;
-        }
-        else
-        {
-            struct header *temp = (header *)(ptr + HEADER_SIZE + size);
-            temp->size = MEMORY_SIZE - size - 2 * HEADER_SIZE;
-            temp->free = false;
-            temp->next = head;
+    if (IS_HEAD_UNINITIALIZED) { // Initializing the memory if this is the first time malloc is being run
 
-            head->next = temp;
-            head->size = size;
+        *head = MEMORY_LENGTH_IN_BYTES; // Letting this "chunk" be the entire array
+        *((int *)head + 1) = 0; // Setting this chunk to be free
+
+    }
+
+    int numberOfBytes = 0;
+    void *allocatedMemory; // The pointer being returned to the USER
+
+    while (numberOfBytes < MEMORY_LENGTH_IN_BYTES) { // Constantly checks if we are within the confines of the memory
+
+        if (IS_HEAD_FREE && size + 8 <= HEAD_SIZE) { // If we have found a location to put the requested memory
+
+            int startingSize = HEAD_SIZE;
+            SET_HEAD_SIZE(size);
+            SET_HEAD_ALLOCATED;
+            allocatedMemory = (void *)(head + 2); // +2 such that the pointer points to actual data and not to the metadata/header
+ 
+            if (startingSize > HEAD_SIZE) { // If the rest of memory has less size than the size of the memory being allocated
+            
+                HEAD_NEXT_SIZE = startingSize - HEAD_SIZE;
+                HEAD_NEXT_IS_FREE;
+
+            }
+
+            return allocatedMemory; // Returns the void pointer to the USER
+
+        } else {
+
+            numberOfBytes += HEAD_SIZE;
+            head = HEAD_NEXT;
+
         }
     }
 
-    while (head != NULL && (!head->free || head->size < size))
-    {
-        if (head->next == firstChunk)
-        {
-            fprintf(stderr, "\nError in %s line %d: No space available \n", FILE, line);
-            return NULL;
-        }
-        head = head->next;
-    }
+    // If could not find a suitable location, then show error and return a NULL pointer to the USER
+    fprintf(stderr, "Error in %p on line %d: Not Enough Space", FILE, line);
+    return NULL;
 
-    head->free = false;
-
-    if (head->size - size > HEADER_SIZE)
-    {
-        void *ptr = (void *)head;
-        struct header *temp = (struct header *)(ptr + HEADER_SIZE + size);
-        temp->size = head->size - size - HEADER_SIZE;
-        temp->free = true;
-        temp->next = head->next;
-
-        head->next = temp;
-        head->size = size;
-    }
-
-    return head + 1;
 }
 
-void myfree(void *ptr, char *FILE, int line)
-{
+/*
+ *
+ * FREE
+ *
+ */
+void myfree(void *ptr, char *FILE, int line) {
 
-    if (ptr == NULL || ptr >= firstChunk + MEMORY_SIZE)
-    {
-        fprintf(stderr, "\nError in %s line %d: Invalid pointer \n", FILE, line);
-        return;
+    int numberOfBytes = 0;
+
+    // Hold two pointers such that we can coalesce chunks as soon as they are freed
+    int *prev = START_OF_MEMORY;
+    int *curr = START_OF_MEMORY;
+
+    while (numberOfBytes <= MEMORY_LENGTH_IN_BYTES - 8) { // Loop through every chunk of memory
+
+        if (MALLOC_POINTER_FOUND) { // If we have found the location of the memory that the USER wants to free
+
+            if (IS_CURR_FREE) {  // If it is already freed, then return an error for attempting to free an already free object
+
+                fprintf(stderr, "Error in %p on line %d: Invalid Free", FILE, line);
+                return;
+
+            } else {
+
+                SET_CURR_FREE; // Make the object free
+
+                if ((numberOfBytes + (*curr)) < MEMORY_LENGTH_IN_BYTES && *(curr + (*curr / 4) + 1) == 0) { *curr += *(curr + (*curr / 4)); }
+
+                if (prev != curr && IS_PREV_FREE) { *prev += *curr; } // Coalesce chunks by pointing the previous chunk to the next available chunk
+
+                return;
+
+            }
+        } else {
+
+            // If the memory was not found this iteration, shift both pointers to the right and the byte indicator
+            prev = curr;
+            curr += *curr / 4;
+            numberOfBytes += *curr;
+
+        }
     }
 
-    struct header *prev = (struct header *)(ptr - HEADER_SIZE);
+    // If no memory was found in the entire list, then return an error to the USER for invalid pointer
+    fprintf(stderr, "Error in %p on line %d: Invalid Pointer", FILE, line);
+    return;
 
-    if (prev->free)
-    {
-        fprintf(stderr, "\nError in %s line %d: Invalid pointer \n", FILE, line);
-        return;
-    }
-    prev->free = true;
-
-    struct header *next = prev->next;
-
-    if (next->free && next != firstChunk)
-    {
-        prev->size += HEADER_SIZE + next->size;
-        prev->next = next->next;
-        next = NULL;
-    }
-
-    header *curr = firstChunk;
-    while (curr != NULL && (!curr->free || curr->next != prev && curr->next == firstChunk))
-    {
-        curr = curr->next;
-    }
-
-    curr->size += HEADER_SIZE + prev->size;
-    curr->next = prev->next;
-    prev = NULL;
 }
--- Projects/My Little Malloc/mymalloc.h
+++ Projects/My Little Malloc/mymalloc.h
@@ -1,19 +1,61 @@
+/*
+ *
+ * INCLUDE STATEMENTS
+ *
+ */
 #include <stddef.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#ifndef _MYMALLOC_H
+/*
+ *
+ * DEFINITIONS
+ *
+ */
 #define _MYMALLOC_H
 #define malloc(s) mymalloc(s, __FILE__, __LINE__)
 #define free(p) myfree(p, __FILE__, __LINE__)
 #define MEMORY_LENGTH 512
+#define NEXT_MULTIPLE_OF_EIGHT size + 7 & -8
+#define SET_HEAD_SIZE(s) *((int *)head) = s + 8
+#define MEMORY_LENGTH 512
+#define MEMORY_LENGTH_IN_BYTES 512 * 8
+#define START_OF_MEMORY (int *)memory
+#define HEAD_SIZE *((int *)head)
+#define SET_HEAD_ALLOCATED *(((int *)head) + 1) = 1
+#define IS_HEAD_FREE *(((int *)head) + 1) == 0
+#define IS_HEAD_UNINITIALIZED HEAD_SIZE == 0 && IS_HEAD_FREE
+#define HEAD_NEXT ((int *)head) + (*(int *)head) / 4
+#define HEAD_NEXT_SIZE *(int *)(HEAD_NEXT)
+#define HEAD_NEXT_IS_FREE *((int *)(HEAD_NEXT) + 1) = 0
+#define IS_CURR_FREE *(curr + 1) == 0
+#define IS_PREV_FREE *(prev + 1) == 0
+#define MALLOC_POINTER_FOUND (void *)(curr + 2) == ptr
+#define SET_CURR_FREE *(curr + 1) = 0;
 
+/*
+ *
+ * METHOD HEADERS
+ *
+ */
 void *mymalloc(size_t size, char *file, int line);
 void myfree(void *ptr, char *file, int line);
 
-typedef struct header
-{
-    bool free;
+/*
+ *
+ * MEMORY ARRAY
+ *
+ */
+static double memory[MEMORY_LENGTH];
+
+/*
+ *
+ * METADATA STRUCT (OLD IMPLEMENTATION)
+ *
+ */
+typedef struct header {
+    int free;
     int size;
     struct header *next;
 } header;
-#endif
+
